023650b126f235ce3aa2b92ef5b4ceb12a4e0312
OMouloud <mouloudouldali@yahoo.fr>
2025-09-17 21:46:45 -0400

    added routers and controllers

--- helpers.php
+++ helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Get he base path
+ * Get the base path
  * 
  * @param string $path
  * @return string
@@ -43,4 +43,31 @@ function loadPartial($name)
   } else {
     echo "Partial '{$name} not found'";
   }
+}
+
+
+/**
+ * Inspect a values
+ * 
+ * @param mixed $value
+ * return void
+ */
+function inspect($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+}
+
+/**
+ * Inspect a values die
+ * 
+ * @param mixed $value
+ * return void
+ */
+function inspectAndDie($value)
+{
+  echo "<pre>";
+  die(var_dump($value));
+  echo "</pre>";
 }
